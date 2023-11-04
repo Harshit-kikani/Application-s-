9377cca094cc4c61bcebe618a6a146a5ff6aad76
harshit <harshitkikani506@gmail.com>
2023-11-04 12:36:01 +0530

    har
    har

---
+++ Quize-app/app/src/main/java/com/example/quize_app/DashbordActivity.java
@@ -0,0 +1,23 @@
+package com.example.quize_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+public class DashbordActivity extends AppCompatActivity {
+
+    CountDownTimer countDownTimer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashbord);
+
+
+
+
+
+    }
+}
